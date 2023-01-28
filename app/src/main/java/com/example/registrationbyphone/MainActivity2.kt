4667e212b93812c69b8c3ac1c6d77fb9dfbb4959
QoriDev02 <amirxalilov196@gmail.com>
2023-01-28 16:52:47 +0500

    version2.0

--- app/src/main/java/com/example/registrationbyphone/MainActivity2.kt
+++ app/src/main/java/com/example/registrationbyphone/MainActivity2.kt
@@ -1,11 +1,51 @@
 package com.example.registrationbyphone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val btnmain2 = findViewById<Button>(R.id.Main2Button)
+        val editmain2 = findViewById<EditText>(R.id.Main2EditText)
+        auth = Firebase.auth
+        auth.setLanguageCode("ru")
+       btnmain2.setOnClickListener {
+           verifyPhoneNumberWithCode(intent.getStringExtra("verificationId"),editmain2.text.toString())
+       }
+
+    }
+    private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+        // [START verify_with_code]
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+        // [END verify_with_code]
+        signInWithPhoneAuthCredential(credential)
+    }
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this,MainActivity3::class.java)
+                    startActivity(intent)
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
     }
 }
